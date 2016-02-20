4e1be24b3d3793ef3f81901c6975158715512665
johanncanon <johanncanon@gmail.com>
2016-02-20 11:13:58 -0500

    servicio de registro del cliente

--- src/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ src/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -38,7 +38,6 @@ public class ApplicationConfig extends Application {
         resources.add(co.com.sms.renta.servicio.impl.ConsultarLugaresImpl.class);
         resources.add(co.com.sms.renta.servicio.impl.ConsultarProveedoresImpl.class);
         resources.add(co.com.sms.renta.servicio.impl.ConsultarServiciosImpl.class);
-        resources.add(co.com.sms.renta.servicio.impl.RegistrarClientesImpl.class);
     }
     
 }
